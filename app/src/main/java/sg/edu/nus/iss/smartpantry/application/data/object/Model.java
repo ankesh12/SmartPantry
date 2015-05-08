0378363a57063c6b1136963907a41392570f8073
ankesh12 <ankesh12@gmail.com>
2015-05-08 14:38:32 +0800

    Added utility file.

---
+++ app/src/main/java/sg/edu/nus/iss/smartpantry/application/data/object/Model.java
@@ -0,0 +1,7 @@
+package sg.edu.nus.iss.smartpantry.application.data.object;
+
+/**
+ * Created by A0134493A on 8/5/2015.
+ */
+public class Model {
+}
---
+++ app/src/main/java/sg/edu/nus/iss/smartpantry/application/util/XMLUtil.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.iss.smartpantry.application.util;
+
+import org.w3c.dom.Document;
+
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/**
+ * Created by A0134493A on 8/5/2015.
+ */
+public class XMLUtil {
+
+
+    public String getElementText(String tag, InputStream stream){
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(stream);
+            doc.getDocumentElement().normalize();
+            stream.close();
+            return doc.getElementsByTagName(tag).item(0).getTextContent();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
