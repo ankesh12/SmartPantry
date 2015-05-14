69e68b74243343b72bbc7b98a061992b6d51142e
Ankesh Khandelwal <ankesh12@gmail.com>
2015-05-14 12:20:50 +0800

    removed the intermediate page

--- app/src/main/java/sg/edu/nus/iss/smartpantry/controller/ItemController.java
+++ app/src/main/java/sg/edu/nus/iss/smartpantry/controller/ItemController.java
@@ -3,6 +3,7 @@ package sg.edu.nus.iss.smartpantry.controller;
 import android.content.Context;
 import android.content.Intent;
 
+import sg.edu.nus.iss.smartpantry.application.util.ScanBarcodeActivity;
 import sg.edu.nus.iss.smartpantry.views.AddItem;
 
 /**
@@ -12,7 +13,7 @@ public class ItemController {
     AddItem sphome = new AddItem();
 
     public Intent addItem(Context context){
-        Intent intent = new Intent(context,AddItem.class);
+        Intent intent = new Intent(context,ScanBarcodeActivity.class);
         return intent;
     }
 }
