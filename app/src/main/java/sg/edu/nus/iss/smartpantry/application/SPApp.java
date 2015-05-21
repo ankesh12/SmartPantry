1568a98ef3d0269a5eec82777131c034f1c4ee55
LaxmiNarasimhaMurthy <narasimha.murthy@live.com>
2015-05-21 11:26:28 +0800

    INternal changes

--- app/src/main/java/sg/edu/nus/iss/smartpantry/application/SPApp.java
+++ app/src/main/java/sg/edu/nus/iss/smartpantry/application/SPApp.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.iss.smartpantry.application;
 
 import android.app.ExpandableListActivity;
-import android.app.ListActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
