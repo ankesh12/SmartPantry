fb2f19b1ca132dc70539dd6349c8d3925c79285f
charan-a0134630r <a0134630@u.nus.edu>
2015-09-24 17:04:43 +0800

    Adding .bat file to copy db from app to sdcard

---
+++ copyDB.bat
@@ -0,0 +1,2 @@
+cd D:\AppData\Local\Android\sdk\platform-tools
+adb -d shell "run-as sg.edu.nus.iss.smartpantry cat /data/data/sg.edu.nus.iss.smartpantry/databases/SmartPantry.sqlite > /sdcard/SmartPantry.sqlite"
